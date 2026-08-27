dd06b45f88fc982feac22de117b8efc53488c6eb
Lan Vo <53800483@nebraska.edu>
2026-08-27 17:21:54 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/*
+ Name: Lan Vo
+ Date: 8/27/2026
+
+ Function: Print my name
+ */
+
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+    printf("Hello world!");
+    printf("My name is Lan Vo");
+
+    return 0;
+ }
